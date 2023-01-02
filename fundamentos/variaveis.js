e88babdb578d732fb7686cbb000a4ba67b13a38e
cadu0007 <carlos.duduilha1@gmail.com>
2023-01-02 12:58:13 -0300

    tipo number aula 4

--- fundamentos/variaveis.js
+++ fundamentos/variaveis.js
@@ -1,2 +1,11 @@
-let texte = "esse e um texto";
-let numero = 1;
+// tipo number aula 4
+
+// nessa aula eu vi como que eu posso somar, multiplicar, dividir e subtrair qualquer resultado
+
+const meunum = 7
+
+const primeironum = 4;
+const segundonum = 3;
+
+const valordesejado = primeironum + segundonum;
+console.log(valordesejado);
