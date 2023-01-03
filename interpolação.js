51760c026f47b6c65318a94c76c3a6e59c6b2668
cadu0007 <carlos.duduilha1@gmail.com>
2023-01-03 16:00:20 -0300

    interpolação

---
+++ interpolação.js
@@ -0,0 +1,11 @@
+// templete literal / interpolação
+//interpolação e bem melhor que concatenaçao 
+
+const nome = "cadu";
+const idade = 2023-2005;
+const cidadeDeNascimento = "ilhabela estado de SP"
+
+// const ap = "olá meu nome é" +nome+ "minha idade é" +idade+ "eu sou de" +cidadeDeNascimento;
+const ap = `meu nome é ${nome} eu tenho ${idade} e eu nasci em ${cidadeDeNascimento}`
+
+console.log(ap);
