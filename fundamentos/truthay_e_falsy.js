23b195b8ed687026bc1958f0f5f7431d27031b29
cadu0007 <carlos.duduilha1@gmail.com>
2023-01-03 11:23:00 -0300

    truthay e falsy

---
+++ fundamentos/truthay_e_falsy.js
@@ -0,0 +1,21 @@
+// nessa aula aprendi que tem outros jeitos em que a linguagem de programaçao fala se e true ou false 
+
+console.log(0 == false);
+console.log(1 == true);
+console.log("" == false);
+
+// tei outros meios tambem porexemplo taythy e falsy
+
+//undefined
+
+// null -> vazio ou nada 
+
+let minhavar;
+let varnull = null;
+
+let numero = 7;
+let txt = arremeço;
+
+console.log(minhavar);
+console.log(varnull);
+
