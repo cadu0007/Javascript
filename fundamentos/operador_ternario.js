fa165ad11589a25883db00e64859bdc08aa82e49
cadu0007 <carlos.duduilha1@gmail.com>
2023-01-03 15:30:24 -0300

    tenario

---
+++ fundamentos/operador_ternario.js
@@ -0,0 +1,18 @@
+// ternario
+// o  ternario e basicamente para voce incurtar o seu codigo, quando não e nescessario usar muitas linhas de codigo para executar uma certa função
+
+const idadeMinima = 18;
+const idadeCliente = 17; 
+
+// if (idadeMinima >= idadeCliente){
+//     console.log("boa negrão vai querer o que?");
+// }else{
+//     console.log("que isso mano, vai tomar um suco");
+// }
+
+                //condição                      //true                          //false
+console.log(idadeMinima >= idadeCliente ? "boa negrão vai querer o que?" : "que isso mano, vai tomar um suco");
+
+
+
+
