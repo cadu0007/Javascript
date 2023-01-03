e4db3906e8b13e4f459b3be7c461fe8a97bcb0a5
cadu0007 <carlos.duduilha1@gmail.com>
2023-01-03 17:51:18 -0300

    expressao de funçao

---
+++ expressão_de_função.js
@@ -0,0 +1,20 @@
+//declaração de função
+//eles não terminaram essa função 
+
+function minhafunçao(param) {
+    //bloco de de codigo 
+}
+
+minhafunçao("param")
+
+//expressão de funçao 
+
+
+console.log(apresentar());
+
+function apresentar() {
+    return "olá"
+}
+
+console.log(soma(1, 1));
+const soma = function(num1, num2) { return num1 +num2 }
