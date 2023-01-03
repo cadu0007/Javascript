f8432faa1bf38f86f766fa3df88f9a928ee4da06
cadu0007 <carlos.duduilha1@gmail.com>
2023-01-03 14:05:02 -0300

    console.log

---
+++ fundamentos/console.Api.js
@@ -0,0 +1,20 @@
+//console.Api
+
+//console.log e nada mais nada menos doque um registro de uma operaçao ou codigo
+//log -> registro
+
+//tratamento de erro!
+//quando nos erramos alguma linha de codigo aparece uma mensagem e essa mensagem foi outra pessoa que escrevel da melhor maneira possivel para que agente entenda 
+
+//meu proprio exemplo tirado do video 
+
+// e senpre interessante usarmos console.log e console.erro para facilitar o encontro do errro 
+
+let negrão = "muito calmo"
+let arremeçoDeTres = "true"
+
+console.log(777);
+console.log(negrão);
+console.log(arremeçoDeTres);
+
+console.error("vixi negrão deu ruim");
