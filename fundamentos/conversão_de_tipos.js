0f5b6713bfa0d6634969b0caeee8eb223e2a274f
cadu0007 <carlos.duduilha1@gmail.com>
2023-01-03 13:00:37 -0300

    conversão de tipos

---
+++ fundamentos/conversão_de_tipos.js
@@ -0,0 +1,15 @@
+//tipo de dados 
+//boleanos
+
+//conversão implicita 
+
+const numero = 7; 
+const numeroString = Number("7");
+
+//Number()
+//String()
+
+console.log(numero + numeroString);
+
+// conversão explicita
+
