bda3b84e7f4eebfe57d4562ca44ecd56ba014475
cadu0007 <carlos.duduilha1@gmail.com>
2023-01-03 15:07:39 -0300

    operadores de comparação

---
+++ fundamentos/operadores_de_comparação.js
@@ -0,0 +1,18 @@
+// == (comparação implicita)
+
+const numero = 7;
+const txt = "7";
+
+console.log(numero === txt);
+
+//typeof
+console.log(typeof numero);
+console.log(typeof txt);
+
+// == só compara o valor 
+// === compara valor e todo tipo de dado 
+
+// comversão explicita 
+
+Number()
+String()
