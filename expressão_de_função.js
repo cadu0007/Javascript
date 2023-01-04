199296ac80df76e6ba4d6b43c57a3c95d6ac1ec6
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2023-01-04 11:24:09 -0300

    Adicionado alguns comentários e explicações
    Adicionado alguns comentários e explicações

--- expressão_de_função.js
+++ expressão_de_função.js
@@ -1,20 +1,57 @@
 //declaração de função
 //eles não terminaram essa função 
 
+let num1 = 1;
+let num2 = 2;
+
 function minhafunçao(param) {
     //bloco de de codigo 
+}
+
+
+function mostrarValoresIniciais(){
+   console.log(num1);
+   console.log(num2);
+}
+
+function calcularSomaVoid(){
+   num1 = num1+1; //2
+   num2 = num2+2; //4
+}
+
+//retorno de string
+function apresentar() {
+    return "olá"
+}
+
+//retorno de number
+function calcularSoma(num1, num2){
+   return num1+num2;
+}
+
+//void
+function mostrarVoid(str){
+    console.log(str);
 }
 
 minhafunçao("param")
 
 //expressão de funçao 
 
-
 console.log(apresentar());
 
-function apresentar() {
-    return "olá"
-}
+mostrarVoid('Hello world');
+
+calcularSomaVoid();
+
+mostrarValoresIniciais(); //2 - 4
+
+let resultadoFinal = calcularSoma(1,2);
+
+//6
+let resultadoSomado = calcularSoma(num1,num2);
 
 console.log(soma(1, 1));
 const soma = function(num1, num2) { return num1 +num2 }
+
+let teste = soma(1, 2);
