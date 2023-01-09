9459a69fcee570027a4339996b4246af949fa59a
cadu0007 <carlos.duduilha1@gmail.com>
2023-01-09 13:01:38 -0300

    ARRAYS

---
+++ JS-Arrays/IT-arrays.js
@@ -0,0 +1,8 @@
+
+// tem outro metodo que vc tambem pode fazer essa mesma conta que é vc fazendo areavel por vareavel, mais esse modo usando arrays e muito mais pratico 
+
+let notas = [7, 9, 8, 6];
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+
+console.log(media);
