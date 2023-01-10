59fe0426fe17ec6d018bb72edb965bd2f24a9f15
cadu0007 <carlos.duduilha1@gmail.com>
2023-01-10 10:05:12 -0300

    lista de duas dimensoes

---
+++ JS-Arrays/lista-duas-dimensoes.js
@@ -0,0 +1,6 @@
+const alunos = ['Nog', 'Nunes', 'Biell', 'Rapha'];
+const medias = [8, 9, 7, 8];
+
+const alunosE_medias = [alunos, medias];
+
+console.log(`Ó aluno da posição da lista é ${alunosE_medias[0][1]} e a sua nota foi ${alunosE_medias[1][1]}`);
