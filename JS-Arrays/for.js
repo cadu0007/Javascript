bd3d90eb428ff7672347138b763729e446b01b73
cadu0007 <carlos.duduilha1@gmail.com>
2023-01-10 11:51:41 -0300

    for

---
+++ JS-Arrays/for.js
@@ -0,0 +1,10 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+
+// primeira exprecao: é executada um unica vez 
+// segunda exprecao: condiçãode execução 
+// terceira execução: é executada sempre ao final do bloco 
+
+for (let indice = 0; indice < numeros.length; indice++)[
+    console.log(numeros[indice])
+]
+
