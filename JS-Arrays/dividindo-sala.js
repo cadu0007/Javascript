6d034fad663e077c8957eae303d7aad5814f2a38
cadu0007 <carlos.duduilha1@gmail.com>
2023-01-10 01:51:24 -0300

    diviindo sala

---
+++ JS-Arrays/dividindo-sala.js
@@ -0,0 +1,34 @@
+const alunos = [
+    "João",
+    "Juliana",
+    "Ana",
+    "Caio",
+    "Lara",
+    "Marjorie",
+    "Guilherme",
+    "Aline",
+    "Fabiana",
+    "Andre",
+    "Carlos",
+    "Paulo",
+    "Bia",
+    "Vivian",
+    "Isable",
+    "vinicios",
+    "Renan",
+    "Renata",
+    "Daisy",
+    "Camilo",
+];
+
+// esse slice e para voce dividir as coisas 
+
+const sala1 = alunos.slice(0, alunos.length / 2);
+const sala2 = alunos.slice(alunos.length / 2);
+
+console.log(sala1);
+console.log(sala2);
+
+
+
+
