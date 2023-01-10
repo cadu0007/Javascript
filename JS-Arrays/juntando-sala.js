266190e865048e1232125d11b239657fe9d3fe2f
cadu0007 <carlos.duduilha1@gmail.com>
2023-01-10 09:41:32 -0300

    juntando sala

---
+++ JS-Arrays/juntando-sala.js
@@ -0,0 +1,8 @@
+const salaPH = ["Alvaro", "Samuel", "Caique", "Pedro"];
+const salaSJ = ["Gabriel", "Matheus", "Arthur", "Dominique"];
+
+// concat significa concatenação e concatenaçao significa unificar ou juntar 
+
+const salasUnidas = salaPH.concat(salaSJ);
+
+console.log(salasUnidas);
