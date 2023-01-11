a031a92cb207a4b57f2ef472a0fcd80243ac4d0f
cadu0007 <carlos.duduilha1@gmail.com>
2023-01-11 00:38:32 -0300

    map

---
+++ JS-Arrays/callbacks.js
@@ -0,0 +1,15 @@
+const nomes = ['nog', 'tuti', 'biell']
+
+nomes.forEach(function(nome){
+    console.log(nome);
+});
+
+nomes.forEach((nome) =>{
+    console.log(nome);
+});
+
+// function imprimirNome(nome){
+//     console.log(nome);
+// }
+
+// nomes.forEach(imprimirNome);
---
+++ JS-Arrays/map.js
@@ -0,0 +1,11 @@
+const notas = [10, 9.5, 8, 7, 6];
+
+const atualizarNotas = notas.map((nota) => {
+    return nota + 1 >= 10 ? 10 : nota + 1;
+});
+
+console.log(atualizarNotas);
+
+
+
+
